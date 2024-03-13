eb9c13fdeb5399d8972385a2bccccc184b9899f6
infinityrbx <131098555+infinityrbx@users.noreply.github.com>
2024-03-13 20:48:13 +0700

    First commit

---
+++ server/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+
+// CONFIG
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json())
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy:"cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit:"30mb", extented:"true"}));
+app.use(bodyParser.urlencoded({limit:"30mb", extented:"true"}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'pubilc/assets')));
+
+// FILE STORAGE
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null,"pubilc/assets");
+    },
+    filename: function(req,file,cb) {
+        cb(null,file.originalname);
+    }
+})
+
+const upload = multer({ storage });
+
+//  ROUTE WITH FILE
+
+app.post("auth/register", upload.single("picture"), register);
+
+// MONGOOSE SETUP
+
+const PORT = process.env.PORT || 6005
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlparser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`))
+})
+.catch((error) => console.log(`${error} did not connect`));
---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 50,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            min: 8,
+        },
+        email: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impression: Number,
+    }, {timestamps: true}
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
