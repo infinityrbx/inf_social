1939fe5dc7e81e77a6c0cb8ff19d16a35909f11a
infinityrbx <131098555+infinityrbx@users.noreply.github.com>
2024-05-29 18:29:53 +0700

    delete demo data injector

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ import postRoutes from "./routes/posts.js";
 import { createPost } from "./controllers/posts.js";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js"; //Import Demo Data
-import Post from "./models/Posts.js"; //Import Demo Data
-import { users, posts } from "./data/index.js";
 
 // CONFIG
 const __filename = fileURLToPath(import.meta.url);
@@ -66,8 +63,5 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server port: ${PORT}`));
-    // Add demo data
-    // User.insertMany(users);
-    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
