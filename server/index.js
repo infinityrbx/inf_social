cefd98929cc9908d3e4c7cf018c1f9c7700dce5b
infinityrbx <aphixay5810@gmail.com>
2024-06-06 00:20:32 +0700

    config for vercel

--- server/index.js
+++ server/index.js
@@ -26,7 +26,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extented: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extented: "true" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // FILE STORAGE
