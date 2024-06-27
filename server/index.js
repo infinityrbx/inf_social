d3c87457e77630cd797186a47430f6c8c21d84c5
infinityrbx <aphixay5810@gmail.com>
2024-06-27 22:14:19 +0700

    config

--- server/index.js
+++ server/index.js
@@ -21,12 +21,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extented: "true" }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extented: "true" }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
@@ -69,10 +68,7 @@ app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 6005;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlparser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server port: ${PORT}`));
   })
